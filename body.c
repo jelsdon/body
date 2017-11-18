8a485f7288c396e5b60577216fc220aa93313761
James Elsdon <Jamesys@gmail.com>
2017-11-18 17:56:28 +1100

    add skel for usage

--- body.c
+++ body.c
@@ -1,3 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PROGRAM_NAME "body"
+
+void usage (int status) {
+  printf ("Usage: %s [FILE]\n", PROGRAM_NAME);
+  printf ("Print the middlish part of the FILE to standard output.\n"); 
+  exit (status);
+}
 
 
 int main (int argc, char **argv) {

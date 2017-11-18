0a4f0953ff2f5d53ec1bdef86b48b6b37cac69a1
James Elsdon <Jamesys@gmail.com>
2017-11-18 20:01:50 +1100

    Add rudimentary file opening

--- body.c
+++ body.c
@@ -10,7 +10,56 @@ void usage (int status) {
 }
 
 
-int main (int argc, char **argv) {
+/* Open the file */
+FILE open_file (char *filename) {
+  FILE *fp;
+  fp=fopen(filename, "r");
+  if (fp == NULL) {
+    printf("Err opening file\n");
+    exit(1);
+  }
+
+  return *fp;
+}
+
+/* http://pubs.opengroup.org/onlinepubs/9699919799/functions/getline.html */
+size_t file_lines (FILE *fp) {
+  char *line = NULL;
+  size_t len = 0;
+  ssize_t read;
   
+  if ( fp == NULL )
+    exit(1);
+
+  while ((read = getline(&line, &len, fp)) != -1) {
+    printf("Retrieved line of length %zu :\n", read);
+    printf("%s", line);
+  }
+
+  if (ferror(fp)) {
+    printf ("err'd out\n");
+    exit(1);
+  }
+
+  free(line);
+
+  return 0;
+
+}
+
+
+int main (int argc, char **argv) {
+  FILE *fp;
+
+  if (argc != 2){
+    printf("Only one file supported\n");
+    exit(1);
+  }
+
+  /* ...try open the file */
+  *fp=open_file(argv[1]);
+  
+
+  fclose(fp);
   return 0;
 }
